8d78cec6ae1251c402688a9100f8ba8a1905d7ee
Rohit Mittal <43905130+rohitm131@users.noreply.github.com>
2021-05-02 19:20:56 +0530

    Update Solution.java

--- DAY 2 Course Schedule III/Solution.java
+++ DAY 2 Course Schedule III/Solution.java
@@ -14,3 +14,36 @@ class Solution {
         
     }
 }
+
+
+//==========================================================================================================================================
+
+class Solution {
+
+    // TC : O(nlogn)
+    // SC : O(n)
+    public int scheduleCourse(int[][] courses) {
+        Arrays.sort(courses, (a,b) -> (a[1] - b[1]));
+
+        // Course duration of the selected ones
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer> ((a,b) -> (b-a));
+
+        int timeElapsed = 0;
+        for(int[] course : courses){
+            if(timeElapsed + course[0] <= course[1]){
+                pq.offer(course[0]);
+                timeElapsed += course[0];
+            } else if(!pq.isEmpty() && pq.peek() > course[0]){
+                timeElapsed = timeElapsed - pq.poll();
+                pq.offer(course[0]);
+                timeElapsed = timeElapsed + course[0];
+            } else {
+                // pq.peek() < course[0]
+                // reject the current course
+            }
+        }
+
+        return pq.size(); // 3
+    }
+}
+
